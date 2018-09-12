01d378971e1f4526c151d60ea2cbab9b8285aea9
AnnaGH1 <annabekkarevich@gmail.com>
2018-09-12 19:40:49 -0400

    Map pins, general corrections

--- js/map.js
+++ js/map.js
@@ -1,43 +1,36 @@
 'use strict';
 
-// postings data
 var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-
 var minPrice = 1000;
 var maxPrice = 1000000;
-
 var minRooms = 1;
 var maxRooms = 5;
-
 var minGuests = 1;
 var maxGuests = 6;
-
-var minLocX = 100; // to be corrected
-var maxLocX = 800; // to be corrected
+var minLocX = 25;
+var maxLocX = document.body.clientWidth - 25;
 var minLocY = 130;
 var maxLocY = 630;
-
 var postingsNumber = 8;
 
+// new avatar and avatars array variables
+var newAvatar;
+var avatars = [];
 
-// new avatar and avatar paths array variables
-var newPath;
-var avatarPaths = [];
-
-// creates one avatar path
-var genarateAvatarPath = function (avatarNumber) {
-  newPath = 'img/avatar/user' + 0 + avatarNumber + '.png';
-  return newPath;
+// creates one avatar
+var genarateAvatar = function (avatarNumber) {
+  newAvatar = 'img/avatars/user' + 0 + avatarNumber + '.png';
+  return newAvatar;
 }
 
-// creates an array of avatar paths
+// creates an array of avatars
 for (var i = 1; i <= postingsNumber; i++) {
-  avatarPaths.push(genarateAvatarPath(i));
+  avatars.push(genarateAvatar(i));
 }
 
 // gets a random integer between two values, inclusive
@@ -47,55 +40,67 @@ var getRandomIntInclusive = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-// new posting and postings array variables
-var newPosting;
 var postings = [];
 
-// function parameters and corresponding arguments
-// param1 - avatarPaths
-// param2 - TITLE
-// param5 - TYPE
-// param6 - CHECKIN
-// param7 - CHECKOUT
-// param8 - FEATURES
-// param9 - PHOTOS
-
-// creates one posting
-var generatePosting = function (param1, param2, param5, param6, param7, param8, param9) {
+// creates one posting and adds it to an array
+var generatePosting = function (avatar, title, type, checkin, checkout, features, photos) {
   var locX = getRandomIntInclusive(minLocX, maxLocX);
   var locY = getRandomIntInclusive(minLocY, maxLocY);
-  newPosting = {
+  var newPosting = {
     author: {
       // should not repeat - to be corrected
-      avatar: param1[0]
+      avatar: avatar[0]
     },
     offer: {
       // should not repeat - to be corrected
-      title: param2[getRandomIntInclusive(0, param2.length - 1)],
+      title: title[getRandomIntInclusive(0, title.length - 1)],
       address: locX.toString(10) + ', ' + locY.toString(10),
       price: getRandomIntInclusive (minPrice, maxPrice),
-      type: param5[getRandomIntInclusive(0, param5.length - 1)],
+      type: type[getRandomIntInclusive(0, type.length - 1)],
       rooms: getRandomIntInclusive (minRooms, maxRooms),
       guests: getRandomIntInclusive (minGuests, maxGuests),
-      checkin: param6[getRandomIntInclusive (0, param6.length - 1)],
-      checkout: param7[getRandomIntInclusive (0, param7.length - 1)],
+      checkin: checkin[getRandomIntInclusive (0, checkin.length - 1)],
+      checkout: checkout[getRandomIntInclusive (0, checkout.length - 1)],
       // should be random length - to be corrected
-      features: param8[0],
+      features: features[0],
       description: '',
-      photos: param9
+      photos: photos
     },
     location: {
       x: locX,
       y: locY
     }
   }
-  return newPosting;
+  postings.push(newPosting);
+  return postings;
 };
 
 // creates an array of posting objects
-for (var i = 1; i <= postingsNumber; i++) {
-  postings.push(generatePosting(avatarPaths, TITLE, TYPE, CHECKIN, CHECKOUT, FEATURES, PHOTOS));
+for (var i = 0; i < postingsNumber; i++) {
+  generatePosting(avatars, TITLE, TYPE, CHECKIN, CHECKOUT, FEATURES, PHOTOS);
 };
 
-// console.log(postings);
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var fragment = document.createDocumentFragment();
+
+var mapPins = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+// creates one pin based on the template
+var createPin = function (postingData) {
+  var pinElement = pinTemplate.cloneNode(true);
+  pinElement.style.left = postingData.location.x - 25 + 'px';
+  pinElement.style.top = postingData.location.y - 70 + 'px';
+  pinElement.querySelector('img').src = postingData.author.avatar;
+  pinElement.querySelector('img').alt = postingData.offer.title;
+  return pinElement;
+}
+
+// creates pins and appends them to fragment element
+for (var j = 0; j < postings.length; j++) {
+  fragment.appendChild(createPin(postings[j]));
+}
+
+// renders pins
+map.appendChild(fragment);
