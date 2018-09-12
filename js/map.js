600d0cb8994c9802f3a6ac3322315144db404ca4
AnnaGH1 <annabekkarevich@gmail.com>
2018-09-12 08:35:52 -0400

    Posting - object generation - variables added

--- js/map.js
+++ js/map.js
@@ -8,8 +8,22 @@ var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
+var minPrice = 1000;
+var maxPrice = 1000000;
+
+var minRooms = 1;
+var maxRooms = 5;
+
+var minGuests = 1;
+var maxGuests = 6;
+
+var minLocX = 100; // to be corrected
+var maxLocX = 800; // to be corrected
+var minLocY = 130;
+var maxLocY = 630;
 
 var postingsNumber = 8;
+
 
 // new avatar and avatar paths array variables
 var newPath;
@@ -33,6 +47,7 @@ var getRandomIntInclusive = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
 // new posting and postings array variables
 var newPosting;
 var postings = [];
@@ -47,7 +62,9 @@ var postings = [];
 // param9 - PHOTOS
 
 // creates one posting
-var generatePosting = function (param1, param2, param5, param6, param7, param9) {
+var generatePosting = function (param1, param2, param5, param6, param7, param8, param9) {
+  var locX = getRandomIntInclusive(minLocX, maxLocX);
+  var locY = getRandomIntInclusive(minLocY, maxLocY);
   newPosting = {
     author: {
       // should not repeat - to be corrected
@@ -55,22 +72,22 @@ var generatePosting = function (param1, param2, param5, param6, param7, param9) 
     },
     offer: {
       // should not repeat - to be corrected
-      title: param2[getRandomIntInclusive(0, 7)],
-      // address: '',
-      price: getRandomIntInclusive (1000, 1000000),
-      type: param5[getRandomIntInclusive(0, 3)],
-      rooms: getRandomIntInclusive (1, 5),
-      guests: getRandomIntInclusive (1, 10),
-      checkin: param6[getRandomIntInclusive (0, 2)],
-      checkout: param7[getRandomIntInclusive (0, 2)],
-    //   features: param8[0],
+      title: param2[getRandomIntInclusive(0, param2.length - 1)],
+      address: locX.toString(10) + ', ' + locY.toString(10),
+      price: getRandomIntInclusive (minPrice, maxPrice),
+      type: param5[getRandomIntInclusive(0, param5.length - 1)],
+      rooms: getRandomIntInclusive (minRooms, maxRooms),
+      guests: getRandomIntInclusive (minGuests, maxGuests),
+      checkin: param6[getRandomIntInclusive (0, param6.length - 1)],
+      checkout: param7[getRandomIntInclusive (0, param7.length - 1)],
+      // should be random length - to be corrected
+      features: param8[0],
       description: '',
       photos: param9
     },
     location: {
-      // arguments are random - to be corrected
-      x: getRandomIntInclusive(100, 800),
-      y: getRandomIntInclusive(100, 800)
+      x: locX,
+      y: locY
     }
   }
   return newPosting;
@@ -78,5 +95,7 @@ var generatePosting = function (param1, param2, param5, param6, param7, param9) 
 
 // creates an array of posting objects
 for (var i = 1; i <= postingsNumber; i++) {
-  postings.push(generatePosting(avatarPaths, TITLE, TYPE, CHECKIN, CHECKOUT, PHOTOS));
+  postings.push(generatePosting(avatarPaths, TITLE, TYPE, CHECKIN, CHECKOUT, FEATURES, PHOTOS));
 };
+
+// console.log(postings);
