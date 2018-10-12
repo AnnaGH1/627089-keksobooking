1b3e72d16344e85a9228ce0b3bb7c00f60d22f5e
AnnaGH1 <annabekkarevich@gmail.com>
2018-10-12 00:01:22 -0400

    Filters applied simultaneously

--- js/filters.js
+++ js/filters.js
@@ -20,8 +20,7 @@
       var elevatorFilter = featuresFilter.querySelector('#filter-elevator');
       var conditionerFilter = featuresFilter.querySelector('#filter-conditioner');
 
-
-      // Adds priceRange property to postings elements - data-preparation.js ?
+      // Adds priceRange property to postings elements
       var addPriceRange = function (element) {
         var priceRange;
         if (element.offer.price < 10000) {
@@ -87,7 +86,6 @@
         }
 
         element.rank = rank;
-        // console.log(index + ' element rank ' + element.rank);
         return element;
       };
 
@@ -111,25 +109,40 @@
         // Remove elements currently rendered
         window.map.removePostings();
 
+        // Variables for current values in filters
+        var typeChoice = typeFilter.value;
+        var priceChoice = priceFilter.value;
+        var roomsChoice = roomsFilter.value;
+        var guestsChoice = guestsFilter.value;
+
+        // Keep postings that match all filters applied
+        var keepMatchingPostings = function (element) {
+          var wifi = window.util.checkArrayContainsElement(element.offer.features, 'wifi');
+          var dishwasher = window.util.checkArrayContainsElement(element.offer.features, 'dishwasher');
+          var parking = window.util.checkArrayContainsElement(element.offer.features, 'parking');
+          var washer = window.util.checkArrayContainsElement(element.offer.features, 'washer');
+          var elevator = window.util.checkArrayContainsElement(element.offer.features, 'elevator');
+          var conditioner = window.util.checkArrayContainsElement(element.offer.features, 'conditioner');
+
+          if (((element.offer.type === typeChoice) || typeChoice === 'any') && ((element.offer.priceRange === priceChoice) || priceChoice === 'any') && ((element.offer.rooms === roomsChoice) || roomsChoice === 'any') && ((element.offer.guests === guestsChoice) || guestsChoice === 'any') && ((wifi && wifiFilter.checked) || !wifiFilter.checked) && ((dishwasher && dishwasherFilter.checked) || !dishwasherFilter.checked) && ((parking && parkingFilter.checked) || !parkingFilter.checked) && ((washer && washerFilter.checked) || !washerFilter.checked) && ((elevator && elevatorFilter.checked) || !elevatorFilter.checked) && ((conditioner && conditionerFilter.checked) || !conditionerFilter.checked)) {
+            return true;
+          }
+          return false;
+        };
+
+        var postingsMatch = postingsData.filter(keepMatchingPostings);
+
         // Add rank property
-        postingsData.forEach(addRank);
+        postingsMatch.forEach(addRank);
 
         // Sort according to rank property and if equal - lower price first
-        postingsData.sort(function (left, right) {
+        postingsMatch.sort(function (left, right) {
           var rankDiff = right.rank - left.rank;
           if (rankDiff === 0) {
             rankDiff = priceComparison(left.offer.price, right.offer.price);
           }
           return rankDiff;
         });
-
-        // Keep postings that match filters applied
-        var keepMatchingPostings = function (element) {
-          if (element.rank > 0) {
-            return true;
-          }
-          return false;
-        };
 
         // Keep first 5 postings max
         var keepTopMatch = function (element, index) {
@@ -139,116 +152,31 @@
           return false;
         };
 
-        var postingsTopMatch = postingsData.filter(keepMatchingPostings).filter(keepTopMatch);
+        // var postingsTopMatch = postingsData.filter(keepMatchingPostings).filter(keepTopMatch);
+        var postingsTopMatch = postingsMatch.filter(keepTopMatch);
 
         // Render elements
         window.map.showPostings(postingsTopMatch);
       };
 
+      var onFilterChange = function () {
+        window.debounce(showFiltered);
+      };
 
       // Filters - event listeners
-      typeFilter.addEventListener('change', showFiltered);
-      priceFilter.addEventListener('change', showFiltered);
-      roomsFilter.addEventListener('change', showFiltered);
-      guestsFilter.addEventListener('change', showFiltered);
-      wifiFilter.addEventListener('change', showFiltered);
-      dishwasherFilter.addEventListener('change', showFiltered);
-      parkingFilter.addEventListener('change', showFiltered);
-      washerFilter.addEventListener('change', showFiltered);
-      elevatorFilter.addEventListener('change', showFiltered);
-      conditionerFilter.addEventListener('change', showFiltered);
+      typeFilter.addEventListener('change', onFilterChange);
+      priceFilter.addEventListener('change', onFilterChange);
+      roomsFilter.addEventListener('change', onFilterChange);
+      guestsFilter.addEventListener('change', onFilterChange);
+      wifiFilter.addEventListener('change', onFilterChange);
+      dishwasherFilter.addEventListener('change', onFilterChange);
+      parkingFilter.addEventListener('change', onFilterChange);
+      washerFilter.addEventListener('change', onFilterChange);
+      elevatorFilter.addEventListener('change', onFilterChange);
+      conditionerFilter.addEventListener('change', onFilterChange);
 
     }, 1000);
   };
 
   getData();
-
-
-  // Mock data - start
-
-  // var TITLE = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
-  // var TYPE = ['palace', 'flat', 'house', 'bungalo'];
-  // var CHECKIN = ['12:00', '13:00', '14:00'];
-  // var CHECKOUT = ['12:00', '13:00', '14:00'];
-  // var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  // var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-  // var PRICE = {
-  //   'min': 1000,
-  //   'max': 1000000
-  // };
-  //
-  // var PROPERTY_CAPACITY = {
-  //   'minRooms': 1,
-  //   'maxRooms': 5,
-  //   'minGuests': 1,
-  //   'maxGuests': 6
-  // };
-  // var LOCATION = {
-  //   'minX': 0,
-  //   'maxX': document.body.clientWidth,
-  //   'minY': 130,
-  //   'maxY': 630
-  // };
-  // var postingsNumber = 3;
-
-  // Creates one avatar path
-  // var generateAvatar = function (avatarNumber) {
-  //   var newAvatar = 'img/avatars/user0' + avatarNumber + '.png';
-  //   return newAvatar;
-  // };
-  //
-  // Creates an array of avatars
-  // var avatars = [];
-  // var createAvatarsArray = function () {
-  //   for (var i = 1; i <= postingsNumber; i++) {
-  //     avatars.push(generateAvatar(i));
-  //   }
-  //   return avatars;
-  // };
-  //
-  // createAvatarsArray();
-  //
-  // Creates one posting
-  // var generatePosting = function (avatar, title, type, checkin, checkout, features, photos) {
-  //   var locX = window.util.getRandomIntInclusive(LOCATION.minX, LOCATION.maxX);
-  //   var locY = window.util.getRandomIntInclusive(LOCATION.minY, LOCATION.maxY);
-  //   var posting = {
-  //     author: {
-  //       avatar: avatar
-  //     },
-  //     offer: {
-  //       title: title,
-  //       address: locX.toString(10) + ', ' + locY.toString(10),
-  //       price: window.util.getRandomIntInclusive(PRICE.min, PRICE.max),
-  //       type: type[window.util.getRandomIntInclusive(0, type.length - 1)],
-  //       rooms: window.util.getRandomIntInclusive(PROPERTY_CAPACITY.minRooms, PROPERTY_CAPACITY.maxRooms),
-  //       guests: window.util.getRandomIntInclusive(PROPERTY_CAPACITY.minGuests, PROPERTY_CAPACITY.maxGuests),
-  //       checkin: checkin[window.util.getRandomIntInclusive(0, checkin.length - 1)],
-  //       checkout: checkout[window.util.getRandomIntInclusive(0, checkout.length - 1)],
-  //       features: window.util.selectRandomNoRepeat(features),
-  //       description: '',
-  //       photos: photos
-  //     },
-  //     location: {
-  //       x: locX,
-  //       y: locY
-  //     }
-  //   };
-  //   return posting;
-  // };
-  //
-  // Creates an array of posting objects
-  // var postings = [];
-  // var createPostingsArray = function () {
-  //   for (var i = 0; i < postingsNumber; i++) {
-  //     postings.push(generatePosting(avatars[i], TITLE[i], TYPE, CHECKIN, CHECKOUT, FEATURES, PHOTOS));
-  //   }
-  //   return postings;
-  // };
-  //
-  // createPostingsArray();
-  // var testData = createPostingsArray();
-  // console.log(testData);
-
-  // Mock data - end
 })();
