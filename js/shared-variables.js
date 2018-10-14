a8aacf45567a550cbeb68ee1dc7e73dc366d99b2
AnnaGH1 <annabekkarevich@gmail.com>
2018-10-14 15:50:37 -0400

    Form - event handlers names, refactoring

--- js/shared-variables.js
+++ js/shared-variables.js
@@ -8,19 +8,6 @@
     'middleToHigh': 50000
   };
 
-  var CHECKIN_TIME = {
-    'option1': '12:00',
-    'option2': '13:00',
-    'option3': '14:00',
-  };
-
-  var CHECKOUT_TIME = {
-    'option1': '12:00',
-    'option2': '13:00',
-    'option3': '14:00',
-  };
-
-
   var pageMain = document.querySelector('main');
   var filtersContainer = document.querySelector('.map__filters-container');
   var filters = document.querySelector('.map__filters');
@@ -34,8 +21,6 @@
   window.sharedVariables = {
     POSTINGS_NUMBER_LIMIT: POSTINGS_NUMBER_LIMIT,
     PRICE_THRESHOLD: PRICE_THRESHOLD,
-    CHECKIN_TIME: CHECKIN_TIME,
-    CHECKOUT_TIME: CHECKOUT_TIME,
     pageMain: pageMain,
     filtersContainer: filtersContainer,
     filters: filters,
